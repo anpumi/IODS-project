1b0f722ae47c3a7b847482b8da6ab505cc5ac598
msnxt <44422941+msnxt@users.noreply.github.com>
2018-11-24 07:54:55 +0200

    Ch5 begun
    After wrestling with this for a couple of days, I left R well alone for a day or so. I came back to this this morning, and I'm finally making some headway!

--- data/create_human.R
+++ data/create_human.R
@@ -49,3 +49,83 @@ human <- read.table("create_human.csv", sep = "," , header=TRUE)
 human
 
 ## ~ ~ End of Data Wrangling ~ ~
+
+## ~ ~ Data Wrangling for Chapter 5 ~ ~
+# Annukka Puotiniemi
+# 21.11.2018
+# Data wrangling exercise
+# This is my script file for chapter 5 data wrangling, continued from last week.
+# Link to the original data source (2 files, that I have already combined to the "human" data found below):
+# http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv
+# http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv
+
+
+# Load the human data into R. Explore the structure and the dimensions of the data. Describe the data briefly.
+
+human <- read.table("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt", sep  =",", header = T)
+str(human) # the data has 195 observations and 19 variables, as it should.
+head(human)
+summary(human)
+names(human)
+
+# This data originates from the United Nations Development Programme. The Human Development Index (HDI) was created
+# to emphasize that, for an assesment of the development of a country, economic growth is not enough as a measure.
+# Instead, people and their capabilities should be the assesment criteria. The HDI is made up of three indices:
+# Life Expectancy Index, Education Index, and GNI Index.
+# More information about the index can be found here: http://hdr.undp.org/en/content/human-development-index-hdi
+# The data combines several indicators, collected from most countries in the world. The descriptions of the
+# variables can be found here: https://raw.githubusercontent.com/TuomoNieminen/Helsinki-Open-Data-Science/master/datasets/human_meta.txt
+
+# 1. Mutate the data: transform the Gross National Income (GNI) variable to numeric 
+
+library(stringr)
+str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric$GNI
+human$GNI <- as.numeric(human$GNI)
+human$GNI
+
+# 2. Exclude unneeded variables: keep only the columns matching the following variable names (described in the meta
+# file above):  "Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F"
+
+# columns to keep
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Life.Exp", "Edu.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+
+# select the 'keep' columns
+human <- select(human, one_of(keep))
+
+# 3. Remove all rows with missing values
+
+# print out a completeness indicator of the 'human' data
+complete.cases(human)
+
+# print out the data along with a completeness indicator as the last column
+data.frame(human[-1], comp = complete.cases(human))
+
+# filter out all rows with NA values
+human <- filter(human, complete.cases(human))
+
+# 4. Remove the observations which relate to regions instead of countries
+
+# look at the last 10 observations
+tail(human, 10)
+
+# last indice we want to keep
+last <- nrow(human) - 7
+
+# choose everything until the last 7 observations
+human <- human[1:last, ]
+
+# 5. Define the row names of the data by the country names and remove the country name column from the data. 
+# The data should now have 155 observations and 8 variables. 
+# add countries as rownames
+rownames(human) <- human$Country
+
+
+# remove the Country variable
+human <- select(human, -Country)
+
+str(human) # 155 observations and 8 variables, correct!
+
+# Save the human data in your data folder including the row names. You can overwrite your old 'human' data. 
+write.csv(human, file = "create_human.csv", row.names=TRUE)
+ 
+## ~ ~ End of Data Wrangling ~ ~
--- install stuff.R
+++ install stuff.R
@@ -2,4 +2,47 @@ ggpairs(Boston, lower = list(combo  =wrap("facethist", bins=20)))
 install.packages("corrplot")
 install.packages("tidyr")
 install.packages("dplyr")
-test
+install.packages("ggplot2")
+install.packages("GGally")
+library(ggplot2)
+library(dplyr)
+library(GGally)
+library(tidyr)
+
+names(km)
+  
+#  [1] "cluster"      "centers"      "totss"        "withinss"    
+#[5] "tot.withinss" "betweenss"    "size"         "iter"        
+#[9] "ifault"  
+
+cluster <- km$cluster
+str(cluster)
+cluster <- as.data.frame(cluster)
+str(cluster)
+str(boston_K2_scaled)
+bonus_data <- merge(boston_K2_scaled, cluster)
+str(bonus_data)
+bonus_data
+cluster
+# access the centers component in the kmeans data
+km_cluster <- as.data.frame(km$cluster)
+# number of rows in the km dataset 
+n <- nrow(km_cluster)
+n
+
+# choose randomly 80% of the rows
+ind <- sample(n,  size = n * 0.8)
+
+# create train set
+train <- km_cluster[ind,]
+
+# create test set 
+test <- km_cluster[-ind,]
+
+# linear discriminant analysis
+lda.fit <- lda(crim ~ ., data = train)
+
+# print the lda.fit object
+lda.fit
+
+matrix_product
